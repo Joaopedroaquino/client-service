f4a030a0b9fd5b9bc09ea1ecacefde8bf7896cb3
joaopedroaquino <aqerhack@gmail.com>
2023-02-22 14:37:58 -0300

    Create todos service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodosModule } from './modules/todos/todos.module';
 import { DatabaseModule } from './shared/infra/db/database.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, TodosModule],
   controllers: [AppController],
   providers: [AppService],
 })
