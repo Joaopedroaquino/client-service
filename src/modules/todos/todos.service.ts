8cf651bfc664eae0b5aa7e7174eebb07f4c9a47b
joaopedroaquino <aqerhack@gmail.com>
2023-02-22 15:02:18 -0300

    Get response data

--- src/modules/todos/todos.service.ts
+++ src/modules/todos/todos.service.ts
@@ -2,20 +2,36 @@ import { HttpService } from "@nestjs/axios";
 import { Injectable } from "@nestjs/common";
 import { ITodo } from "./interfaces/todo.interface";
 
+
 @Injectable()
 export class TodosService {
-  constructor(private readonly httpService: HttpService) {}
 
-  async getAllTodos() {
-    let todos: ITodo[] = [];
+    constructor(private readonly httpService: HttpService) {
 
-    const url = 'https://jsonplaceholder.typicode.com/todos';
-
-    const { status, data } = await this.httpService.get<ITodo[]>(url).toPromise();
-    if (status === 200) {
-      todos = data;
     }
 
-    return todos;
-  }
+
+    async getAllTodos() {
+        let todos: ITodo[] = [];
+
+        const url = 'https://jsonplaceholder.typicode.com/todos';
+
+        const { status, data } = await this.httpService.get<ITodo[]>(url).toPromise();
+        if (status === 200) {
+            todos = data;
+        }
+        console.log(todos)
+
+        return todos;
+    }
+
+    public async getAllData() {
+        const url = 'https://jsonplaceholder.typicode.com/todos';
+        let result = await this.httpService.get<ITodo[]>(url).toPromise();
+        return result
+        
+    }
+
+
+
 }
