3fa13d8e84802ad9639d8918989277878b2a1cb3
joaopedroaquino <aqerhack@gmail.com>
2023-02-22 17:11:12 -0300

    Get response data

--- src/modules/todos/todos.service.ts
+++ src/modules/todos/todos.service.ts
@@ -5,23 +5,16 @@ import { ITodo } from "./interfaces/todo.interface";
 
 @Injectable()
 export class TodosService {
-
-    constructor(private readonly httpService: HttpService) {
-
-    }
-
+    constructor(private readonly httpService: HttpService) { }
 
     async getAllTodos() {
         let todos: ITodo[] = [];
-
         const url = 'https://jsonplaceholder.typicode.com/todos';
-
         const { status, data } = await this.httpService.get<ITodo[]>(url).toPromise();
         if (status === 200) {
             todos = data;
         }
-        console.log(todos)
-
+        console.log(todos);
         return todos;
     }
 
@@ -29,7 +22,6 @@ export class TodosService {
         const url = 'https://jsonplaceholder.typicode.com/todos';
         let result = await this.httpService.get<ITodo[]>(url).toPromise();
         return result
-        
     }
 
 
